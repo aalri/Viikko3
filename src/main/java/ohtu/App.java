9dc9a3d0fda03e2efca6d5d91beb610e7185f1d2
aalri <riku.alho@kolumbus.fi>
2016-04-10 03:00:18 +0300

    checkstylen tuho

--- src/main/java/ohtu/App.java
+++ src/main/java/ohtu/App.java
@@ -40,6 +40,12 @@ public class App {
             
 
         }
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                for (int k = 0; k < 10; k++) {
+                }
+            }
+        }
     }
     
     public void listen(String command){
